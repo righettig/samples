41f92e362773207157c5d445e981fc565062de51
righettig <righetti.giacomo@live.com>
2014-09-11 11:52:39 +0200

    Initial commit. Added the library we're going to have available as a nuget package.

---
+++ NuGetPackage(r)/Foo.MyLibrary/AwesomeClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Foo.MyLibrary
+{
+    public static class AwesomeClass
+    {
+        public static void AwesomeMethod()
+        {
+            Console.WriteLine("And here it is.. an awesome message!");
+        }
+    }
+}
