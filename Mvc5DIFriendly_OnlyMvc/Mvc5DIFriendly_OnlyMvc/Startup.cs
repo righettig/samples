365aadfc28cf8dbb670369c4a5237214880a5a26
righettig <righetti.giacomo@live.com>
2014-10-09 11:13:15 +0200

    Initial commit. Updated Nuget packages.

---
+++ Mvc5DIFriendly_OnlyMvc/Mvc5DIFriendly_OnlyMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5DIFriendly_OnlyMvc.Startup))]
+namespace Mvc5DIFriendly_OnlyMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
